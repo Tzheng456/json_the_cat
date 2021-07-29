87d6e78c90862a0c78857a3e3515e3f4ad8befa2
Tzheng456 <Tzheng456@gmail.com>
2021-07-29 20:55:16 +0000

    implemented breedFetcher and edge cases

---
+++ breedFetcher.js
@@ -0,0 +1,23 @@
+let request = require("request");
+let breedName = process.argv[2];
+
+request(
+  `https://api.thecatapii.com/v1/breeds/search?q=${breedName}`,
+  (error, response, body) => {
+    //logs error if request fails
+    if (error !== null) {
+      console.log("error:", error);
+      return;
+    } else {
+      //fetch and parse body
+      const data = JSON.parse(body);
+      //if the JSON object parsed from body returns an empty array
+      if (data.length === 0) {
+        console.log("Breed not found!");
+        return;
+      }
+      //print the description for ${breedName}
+      console.log(data[0].description);
+    }
+  }
+);
