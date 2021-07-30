e3601ac61d55d29ebf7cde2fc24e67387c7bbf24
Tzheng456 <Tzheng456@gmail.com>
2021-07-30 02:02:57 +0000

    refactor breedFetcher to be imported to index, added tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,17 @@
 let request = require("request");
-let breedName = process.argv[2];
 
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
-  (error, response, body) => {
-    //logs error if request fails
-    if (error !== null) {
-      console.log("error:", error);
-      return;
-    } else {
-      //fetch and parse body
+const fetchBreedDescription = function(breedName, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    (error, response, body) => {
       const data = JSON.parse(body);
-      //if the JSON object parsed from body returns an empty array
-      if (data.length === 0) {
-        console.log("Breed not found!");
+      if (data.length === 0 || breedName === undefined) {
+        callback(Error(`Breed not found!: ${breedName}`), null);
         return;
       }
-      //print the description for ${breedName}
-      console.log(data[0].description);
+      callback(null, data[0].description);
     }
-  }
-);
+  );
+};
+
+module.exports = { fetchBreedDescription };
